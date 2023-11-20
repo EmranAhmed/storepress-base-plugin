2136196d06e6e8cc0855b508616972d77920a24c
Emran Ahmed <emran.bd.08@gmail.com>
2023-11-21 01:21:41 +0600

    update render

--- src/block-01/render.php
+++ src/block-01/render.php
@@ -1,9 +1,12 @@
 <?php
+
+	defined( 'ABSPATH' ) || die( 'Keep Silent' );
+	
 	/**
 	 * Dynamic Block Template.
-	 * @global   array  $attributes (array)    -  A clean associative array of block attributes.
-	 * @global   array  $block      (WP_Block) - The block instance. All the block settings and attributes.
-	 * @global   string $content    (string)   - The block inner HTML (usually empty unless using inner blocks).
+	 * @global   array    $attributes -  A clean associative array of block attributes.
+	 * @global   WP_Block $block      - The block instance. All the block settings and attributes.
+	 * @global   string   $content    - The block inner HTML (usually empty unless using inner blocks).
 	 */
 
 
@@ -16,20 +19,18 @@
 	);
 
 	$styles = array(
-		'--css-variable' => $attributes['x'] . '%',
+		'--css-variable' => $attributes[ 'x' ] . '%',
 	);
 
 	$context = $block->context;
 
-	$wrapper_attributes = get_block_wrapper_attributes(
-		array(
-			'class' => esc_attr( storepress_base_plugin()->get_blocks()->css_classes( $classes ) ),
-			'style' => esc_attr( storepress_base_plugin()->get_blocks()->inline_styles( $styles ) ),
-		)
-	);
+	$wrapper_attributes = get_block_wrapper_attributes( array(
+															'class' => esc_attr( storepress_base_plugin()->get_blocks()->css_classes( $classes ) ),
+															'style' => esc_attr( storepress_base_plugin()->get_blocks()->inline_styles( $styles ) ),
+														) );
 
 	$allowed_html = storepress_base_plugin()->get_blocks()->get_kses_allowed_html();
-	?>
+?>
 <div <?php echo wp_kses_data( $wrapper_attributes ); ?>>
 	<h1>StorePress Base Block - render.php</h1>
 	<?php echo wp_kses( $content, $allowed_html ); ?>
